a62a5f95d5631206f698c7251cc6e6b3d3c36ae3
Matcheme Elie <eliematcheme@gmail.com>
2019-10-15 19:12:23 +0200

    Modificationde la version du Plugin

--- onlinePay.php
+++ onlinePay.php
@@ -3,7 +3,7 @@
 Plugin Name: Payement Orange Money wordpress Cameroun 
 Plugin URI: http://smicam.com/service
 Description: Un plugin pour le payement Orange Money Cameroun
-Version: 0.1
+Version: 0.2
 Author: Matcheme Elie
 Author URI: http://smicam.com/service
 */
